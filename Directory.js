2c6bc5301160a9eacfb6b4b2031509644e05f307
CarterJWallace <43143694+CarterJWallace@users.noreply.github.com>
2020-02-25 09:29:12 -0500

    Update Directory.js
    Replaced var statements with let statements. Moved elements into a function that only runs after the window has finished loading. Currently untested.

--- Directory.js
+++ Directory.js
@@ -1,44 +1,47 @@
-var level1 = document.getElementsByClassName("level1");
-var level2 = document.getElementsByClassName("level2");
+function start() {
+	let level1 = document.getElementsByClassName("level1");
+	let level2 = document.getElementsByClassName("level2");
 
-var i;
+	let i;
 
-for(i = 0; i < level1.length; i++){
-	level1[i].addEventListener("click", function(){
-		var label = this.innerHTML;
-		if (label[0] == '+'){
-			label = label.replace("+", "-");
-			this.innerHTML = label;
-		} else{
-			label = label.replace("-", "+");
-			this.innerHTML = label;
-		}
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if(content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else{
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
+	for(i = 0; i < level1.length; i++){
+		level1[i].addEventListener("click", function(){
+			let label = this.innerHTML;
+			if (label[0] == '+'){
+				label = label.replace("+", "-");
+				this.innerHTML = label;
+			} else{
+				label = label.replace("-", "+");
+				this.innerHTML = label;
+			}
+			this.classList.toggle("active");
+			let content = this.nextElementSibling;
+			if(content.style.maxHeight){
+				content.style.maxHeight = null;
+			} else{
+				content.style.maxHeight = content.scrollHeight + "px";
+			}
+		});
+	}
+
+	for(i = 0; i < level2.length; i++){
+		level2[i].addEventListener("click", function(){
+			let label = this.innerHTML;
+			if (label[0] == '+'){
+				label = label.replace("+", "-");
+				this.innerHTML = label;
+			} else{
+				label = label.replace("-", "+");
+				this.innerHTML = label;
+			}
+			this.classList.toggle("active");
+			let content = this.nextElementSibling;
+			if(content.style.maxHeight){
+				content.style.maxHeight = null;
+			} else{
+				content.style.maxHeight = content.scrollHeight + "px";
+			}
+		});
+	}
 }
-
-for(i = 0; i < level2.length; i++){
-	level2[i].addEventListener("click", function(){
-		var label = this.innerHTML;
-		if (label[0] == '+'){
-			label = label.replace("+", "-");
-			this.innerHTML = label;
-		} else{
-			label = label.replace("-", "+");
-			this.innerHTML = label;
-		}
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if(content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else{
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	});
-}
+window.addEventListener("load", start, false);
